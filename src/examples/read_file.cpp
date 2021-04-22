23dd11cff773ab81b289bc0ceb1d332937485c40
bernardo <bernardo.fichera@gmail.com>
2021-04-22 10:11:36 +0200

    Added example of the new reading feature.

--- src/examples/read_file.cpp
+++ src/examples/read_file.cpp
@@ -6,8 +6,18 @@ int main(int argc, char const* argv[])
 {
     FileManager io_manager;
 
-    // Load data
-    Eigen::MatrixXd X = io_manager.read<Eigen::MatrixXd>("rsc/joints_icub.csv");
+    Eigen::MatrixXd X = io_manager.setFile("rsc/torus_mesh.mesh").read<Eigen::MatrixXd>("elements", 2),
+                    Y = io_manager.setFile("rsc/joints_icub.csv").read<Eigen::MatrixXd>();
+
+    std::cout << "First file begin" << std::endl;
+    std::cout << X.block(0, 0, 5, 5) << std::endl;
+    std::cout << "First file end" << std::endl;
+    std::cout << X.block(X.rows() - 5, 0, 5, 5) << std::endl;
+
+    std::cout << "Second file" << std::endl;
+    std::cout << Y.block(0, 0, 5, 5) << std::endl;
+    std::cout << "Second file end" << std::endl;
+    std::cout << Y.block(Y.rows() - 5, Y.cols() - 5, 5, 5) << std::endl;
 
     return 0;
 }
