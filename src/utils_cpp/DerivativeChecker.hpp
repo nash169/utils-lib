cbf72a055b3ac041b8a02851de8d2a2efb0b661e
bernardo <bernardo.fichera@gmail.com>
2021-07-13 10:32:19 +0200

    Made input of checkGradient non-constant.

--- src/utils_cpp/DerivativeChecker.hpp
+++ src/utils_cpp/DerivativeChecker.hpp
@@ -37,8 +37,45 @@ namespace utils_cpp {
             return M;
         }
 
+        // template <typename Function, typename Gradient>
+        // bool checkGradient(const Function& f, const Gradient& g)
+        // {
+        //     // Init G
+        //     _G = Eigen::VectorXd::Zero(_res);
+
+        //     // Generate log spaced perturbation intensities
+        //     _t = Eigen::VectorXd::LinSpaced(_res, -8, 0);
+        //     for (size_t i = 0; i < _t.rows(); i++)
+        //         _t(i) = std::pow(10, _t(i));
+
+        //     // Generate random test point and perturbation direction
+        //     Eigen::VectorXd x = Eigen::VectorXd::Random(_dim), v = Eigen::VectorXd::Random(_dim);
+
+        //     // Calculate first order Taylor expansion
+        //     for (size_t i = 0; i < _t.rows(); i++)
+        //         _G(i) = std::abs(f(x + _t(i) * v) - f(x) - _t(i) * g(x).dot(v));
+
+        //     // Transform to log space
+        //     _t = _t.array().log();
+        //     _G = _G.array().log();
+
+        //     // Calculate mean slope for central values
+        //     double mean_slope = 0, num_points = 0;
+
+        //     for (size_t i = 20; i < _res - 20; i++) {
+        //         mean_slope = mean_slope + (_G(i + 1) - _G(i)) / (_t(i + 1) - _t(i));
+        //         num_points++;
+        //     }
+
+        //     mean_slope /= num_points;
+
+        //     std::cout << "First order Taylor expansion slope: " << mean_slope << " - It should be approximately equal to 2.0" << std::endl;
+
+        //     return (std::abs(mean_slope - 2.0) <= 1e-3) ? true : false;
+        // }
+
         template <typename Function, typename Gradient>
-        bool checkGradient(const Function& f, const Gradient& g)
+        bool checkGradient(Function f, Gradient g)
         {
             // Init G
             _G = Eigen::VectorXd::Zero(_res);
