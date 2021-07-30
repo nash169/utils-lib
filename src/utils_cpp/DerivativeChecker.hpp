68621a915680f5c22a32fac8a38532042dc1fbe1
Bernardo <bernardo.fichera@gmail.com>
2021-07-30 11:51:24 +0200

    Overloaded checkGradient to accept as in input initial point and perturbation direction.

--- src/utils_cpp/DerivativeChecker.hpp
+++ src/utils_cpp/DerivativeChecker.hpp
@@ -75,6 +75,40 @@ namespace utils_cpp {
         // }
 
         template <typename Function, typename Gradient>
+        bool checkGradient(Function f, Gradient g, const Eigen::VectorXd& x, const Eigen::VectorXd& v)
+        {
+            // Init G
+            _G = Eigen::VectorXd::Zero(_res);
+
+            // Generate log spaced perturbation intensities
+            _t = Eigen::VectorXd::LinSpaced(_res, -8, 0);
+            for (size_t i = 0; i < _t.rows(); i++)
+                _t(i) = std::pow(10, _t(i));
+
+            // Calculate first order Taylor expansion
+            for (size_t i = 0; i < _t.rows(); i++)
+                _G(i) = std::abs(f(x + _t(i) * v) - f(x) - _t(i) * g(x).dot(v));
+
+            // Transform to log space
+            _t = _t.array().log();
+            _G = _G.array().log();
+
+            // Calculate mean slope for central values
+            double mean_slope = 0, num_points = 0;
+
+            for (size_t i = 20; i < _res - 20; i++) {
+                mean_slope = mean_slope + (_G(i + 1) - _G(i)) / (_t(i + 1) - _t(i));
+                num_points++;
+            }
+
+            mean_slope /= num_points;
+
+            std::cout << "First order Taylor expansion slope: " << mean_slope << " - It should be approximately equal to 2.0" << std::endl;
+
+            return (std::abs(mean_slope - 2.0) <= 1e-3) ? true : false;
+        }
+
+        template <typename Function, typename Gradient>
         bool checkGradient(Function f, Gradient g)
         {
             // Init G
