cdfe2d4c5f38e6b16a5917012a98b97446ff7f3f
bernardo <bernardo.fichera@gmail.com>
2022-01-28 14:08:18 +0100

    Added license applier.

---
+++ scripts/license.py
@@ -0,0 +1,139 @@
+#! /usr/bin/env python
+# encoding: utf-8
+#
+#    This file is part of utils-lib.
+#
+#    Copyright (c) 2020, 2021, 2022 Bernardo Fichera <bernardo.fichera@gmail.com>
+#
+#    Permission is hereby granted, free of charge, to any person obtaining a copy
+#    of this software and associated documentation files (the "Software"), to deal
+#    in the Software without restriction, including without limitation the rights
+#    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#    copies of the Software, and to permit persons to whom the Software is
+#    furnished to do so, subject to the following conditions:
+#
+#    The above copyright notice and this permission notice shall be included in all
+#    copies or substantial portions of the Software.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#    SOFTWARE.
+
+import fnmatch
+import os
+import shutil
+
+
+license = '''This file is part of utils-lib.
+
+Copyright (c) 2020, 2021, 2022 Bernardo Fichera <bernardo.fichera@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.'''
+
+
+def make_dirlist(folder, extensions):
+    matches = []
+    for root, _, filenames in os.walk(folder):
+        for ext in extensions:
+            for filename in fnmatch.filter(filenames, '*' + ext):
+                matches.append(os.path.join(root, filename))
+    return matches
+
+
+def insert_header(fname, prefix, postfix, license, kept_header=[]):
+    input = open(fname, 'r')
+    ofname = '/tmp/' + fname.split('/')[-1]
+    output = open(ofname, 'w')
+
+    for line in kept_header:
+        output.write(line + '\n')
+
+    output.write(prefix + '\n')
+
+    has_postfix = len(postfix) > 0
+
+    my_prefix = prefix
+
+    if has_postfix:
+        my_prefix = ''
+    for line in license.split('\n'):
+        if len(line) > 0:
+            output.write(my_prefix + '    ' + line + '\n')
+        else:
+            output.write(my_prefix + '\n')
+
+    if has_postfix:
+        output.write(postfix + '\n')
+
+    in_header = False
+
+    for line in input:
+        header = len(
+            list(filter(lambda x: x == line[0:len(x)], kept_header))) != 0
+        check_prefix = (line[0:len(prefix)] == prefix)
+        check_postfix = (has_postfix and (line[0:len(postfix)] == postfix))
+        if check_prefix and has_postfix:
+            in_header = True
+        if check_postfix:
+            in_header = False
+        if (not in_header) and (not check_prefix) and (not header) and (not check_postfix):
+            output.write(line)
+
+    output.close()
+    shutil.move(ofname, fname)
+
+
+def insert(directory):
+    # C/C++
+    cpp = make_dirlist(directory, ['.hpp', '.cpp', '.h', '.c', '.cc'])
+    for i in cpp:
+        insert_header(i, '/*', '*/', license)
+
+    # Python
+    py = make_dirlist(directory, ['.py'])
+    header = ['#! /usr/bin/env python', '# encoding: utf-8']
+    for i in py:
+        insert_header(
+            i, '#', '', license, header)
+
+    # CMake (metapackages should not have any comments)
+    cmake = make_dirlist(directory, ['CMakeLists.txt'])
+    for i in cmake:
+        insert_header(i, '#', '', license)
+
+    # XML/URDF
+    xml_urdf = make_dirlist(
+        directory, ['.xml', '.urdf', '.xacro', '.launch'])
+    header = ['<?xml version="1.0"?>']
+
+    for i in xml_urdf:
+        insert_header(
+            i, '<!--', '-->', license, header)
+
+
+if __name__ == '__main__':
+    # Insert license in the sources
+    insert('src')
+
+    # Insert license in the scripts
+    insert('scripts')
